8f029ff2c82e040a119505b967876b3605b67b57
Rafal Wizen <rafal.wizen@gmail.com>
2025-01-22 19:31:57 +0100

    Move files from React project and setup project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Home from "./pages/Home";
+// import Contact from "./pages/Contact";
+//
+// const App = () => {
+//     return (
+//         <Router basename="/sweet-creations">
+//             <div className="min-h-screen bg-background">
+//                 <Navbar />
+//                 <Routes>
+//                     <Route path="/" element={<Home />} />
+//                     <Route path="/contact" element={<Contact />} />
+//                 </Routes>
+//             </div>
+//         </Router>
+//     );
+// };
+//
+// export default App;
