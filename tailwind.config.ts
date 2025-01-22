d1b68d413fa46c33ce9f1ac1aac95af8c5af60f8
Rafal Wizen <rafal.wizen@gmail.com>
2025-01-22 20:05:10 +0100

    Fix tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,25 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#FB7185",
+        },
+        accent: {
+          DEFAULT: "#FBBF24",
+        },
+        background: {
+          DEFAULT: "#F9FAFB",
+        },
+      },
+      fontFamily: {
+        greatVibes: ['GreatVibes', 'cursive'],
+        lora: ['LoraItalic', 'serif'],
       },
     },
   },
